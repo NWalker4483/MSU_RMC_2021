5eef19c347dad07de31c52e9644077e912206757
NWalker4483 <niwal7@morgan.edu>
2021-05-10 23:00:33 -0400

    udev updates

--- udev.sh
+++ udev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo  'SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout", SYMLINK+="arduino_nano"' >/etc/udev/rules.d/arduino.rules
+echo  'SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", MODE:="0777", GROUP:="dialout", SYMLINK+="arduino_nano"' >/etc/udev/rules.d/arduino.rules
 
 sudo adduser $USER dialout
 
